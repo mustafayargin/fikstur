9beab2293bb2d77f9a71b27375de40e8d58d0b2e
mustafayargin <mustafayargin@gmail.com>
2026-05-04 12:27:13 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -24,10 +24,16 @@ const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
   console.log("[FCM SW] Arka plan bildirimi geldi:", payload);
 
-  const title = payload?.notification?.title || "Tahmin Paneli";
+  const title =
+  payload?.data?.title ||
+  payload?.notification?.title ||
+  "Tahmin Paneli";
 
   const options = {
-    body: payload?.notification?.body || "Yeni bildirimin var.",
+    body:
+  payload?.data?.body ||
+  payload?.notification?.body ||
+  "Yeni bildirimin var.",
     icon: "./icons/icon-192.png",
     badge: "./icons/icon-192.png",
     data: {
